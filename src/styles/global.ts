00a81edc7d7540049adf9eac010cce0bd7d6faaa
Dtesch9 <alucard.s8123@gmail.com>
2020-04-21 21:17:03 -0300

    Global Styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -15,7 +15,7 @@ export default createGlobalStyle`
   }
 
   body {
-    background: #f0f0f5 url(${githubBackground});
+    background: #f0f0f5 url(${githubBackground}) no-repeat 70% top;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
