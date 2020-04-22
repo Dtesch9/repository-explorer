aa37dd4d7b4ff9c12bebc8f843c627ad67e664e4
Dtesch9 <alucard.s8123@gmail.com>
2020-04-22 11:09:27 -0300

    Dashboard: Styles

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,48 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-// import { Container } from './styles';
+import logoImg from '../../assets/logo.svg';
 
-const Dashboard: React.FC = () => <h1>Dashboard</h1>;
+import { Title, Form, Repositories } from './styles';
+
+const Dashboard: React.FC = () => (
+  <>
+    <img src={logoImg} alt="Logo" />
+    <Title>Explore repositórios no Github</Title>
+
+    <Form>
+      <input placeholder="Digite o nome do repositório" />
+      <button type="submit">Pesquisar</button>
+    </Form>
+
+    <Repositories>
+      <a href="/">
+        <img
+          src="https://api.adorable.io/avatars/55/abott@adorable.png"
+          alt="Douglas Tesch"
+        />
+        <div>
+          <strong>Dtesch/fastfeet</strong>
+          <p>Best delivery option you have ever known</p>
+        </div>
+
+        <FiChevronRight size={24} />
+      </a>
+
+      <a href="/">
+        <img
+          src="https://api.adorable.io/avatars/55/abott@adorable.png"
+          alt="Douglas Tesch"
+        />
+        <div>
+          <strong>Dtesch/fastfeet</strong>
+          <p>Best delivery option you have ever known</p>
+        </div>
+
+        <FiChevronRight size={24} />
+      </a>
+    </Repositories>
+  </>
+);
 
 export default Dashboard;
